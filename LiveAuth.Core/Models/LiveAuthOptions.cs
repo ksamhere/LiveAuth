d2bfd7f2943935a70e71628cc8f482c82de5f934
kpandian <kpandian@verityconsulting.com>
2026-03-05 10:00:21 -0500

    RoleClaimType in LiveAuthOption modified according to code review

--- LiveAuth.Core/Models/LiveAuthOptions.cs
+++ LiveAuth.Core/Models/LiveAuthOptions.cs
@@ -1,10 +1,12 @@
+using System.Security.Claims;
+
 namespace LiveAuth.Core.Models
 {
     public class LiveAuthOptions
     {
         public string SessionIdClaimType { get; set; } = "sid";
         public string VersionClaimType { get; set; } = "ver";
-        public string RoleClaimType { get; set; } = "role";
+        public string RoleClaimType { get; set; } = ClaimTypes.Role;
         public bool OverrideRoleFromSession { get; set; } = true;
     }
 }
